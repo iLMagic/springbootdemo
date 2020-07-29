c36048a59f22e75b0b33344aa4a8221ede2c39f7
hedahai@bmsoft.com.cn <hedahai@bmsoft.com.cn>
2020-07-29 17:02:16 +0800

    12313

--- src/main/java/com/example/springbootdemo/mapper/UserMapper.java
+++ src/main/java/com/example/springbootdemo/mapper/UserMapper.java
@@ -1,15 +1,15 @@
 package com.example.springbootdemo.mapper;
 
-
-import com.example.springbootdemo.domain.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
-
-@Mapper
+//
+//import com.example.springbootdemo.domain.User;
+////import org.apache.ibatis.annotations.Mapper;
+////import org.apache.ibatis.annotations.Select;
+//
+//import java.util.List;
+//
+//@Mapper
 public interface UserMapper {
-
-    @Select("select * from user;")
-    List<User> getAllUser();
+//
+//    @Select("select * from user;")
+//    List<User> getAllUser();
 }
